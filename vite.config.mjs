941496c0b8c3cf424d806686a8cfd75b1bb3f417
Saran-Jagadeesan-Uma <jagadeesanuma.s@northeastern.edu>
2025-11-07 01:14:53 -0500

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,22 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Saran-Portfolio/",
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('lottie-web') || id.includes('react-lottie-player')) {
-              return 'vendor-lottie';
+          if (id.includes("node_modules")) {
+            if (id.includes("lottie-web") || id.includes("react-lottie-player")) {
+              return "vendor-lottie";
             }
-            if (id.includes('three') || id.includes('@react-three')) {
-              return 'vendor-three';
+            if (id.includes("three") || id.includes("@react-three")) {
+              return "vendor-three";
             }
-            return 'vendor';
+            return "vendor";
           }
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
